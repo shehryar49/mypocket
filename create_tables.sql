4a673c3969b8053663d910a67cf9979f64e9bfb2
Shahryar Ahmad <shehryarshahid49@gmail.com>
2025-01-29 22:36:53 +0500

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -5,7 +5,7 @@ create table passwords(id SERIAL PRIMARY KEY,userid int NOT NULL,email VARCHAR(1
 --parentid is -1 for root folder
 create table filesystem(id SERIAL PRIMARY KEY,userid int,rname VARCHAR(255),rtype int,parentid int);
 
-create table acl(id SERIAL PRIMARY KEY,sharedBy int,ownername VARCHAR(50),sharedTo int,resourceid int,boolean write_access);
+create table acl(id SERIAL PRIMARY KEY,sharedBy int,ownername VARCHAR(50),sharedTo int,resourceid int,write_access boolean);
 
 create table otp(userid INT NOT NULL,code int,created_at DATE);
 
